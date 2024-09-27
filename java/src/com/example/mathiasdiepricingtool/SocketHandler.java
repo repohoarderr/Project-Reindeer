35239b6cff1cb62c467c4a53241e5516ed264559
LongA10 <andrew.joaquin@yahoo.com>
2024-09-27 11:11:01 -0500

    Initial Commit

---
+++ java/src/com/example/mathiasdiepricingtool/SocketHandler.java
@@ -0,0 +1,14 @@
+package com.example.mathiasdiepricingtool;
+import java.io.IOException;
+public class SocketHandler {
+
+
+
+  public static void attemptConnection(int port) throws IOException {
+    try {
+    }catch(Exception e){
+
+    }
+  }
+
+}
