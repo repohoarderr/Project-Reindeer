2d75b2750b883b29179be0cbfe1c5b82e643fd41
LongA10 <andrew.joaquin@yahoo.com>
2024-10-09 10:54:49 -0500

    Changed header response

--- src/main/java/com/example/elk/FileUploadServlet.java
+++ src/main/java/com/example/elk/FileUploadServlet.java
@@ -4,12 +4,16 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+import org.json.simple.*;
+
 import org.glassfish.jersey.*;
 
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
+
 @WebServlet(name = "FileUploadServlet", urlPatterns = { "/fileuploadservlet" })
 @MultipartConfig(
         fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
@@ -19,6 +23,10 @@ import java.io.InputStream;
 public class FileUploadServlet extends HttpServlet {
 
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//    filterChain.doFilter(servletRequest, servletResponse);
     String path = "/Users/andrewlong/Documents/IntelliJ/elk/html/Front-End/";
     /* Receive file uploaded to the Servlet from the HTML5 form */
     Part filePart = request.getPart("file");
@@ -31,6 +39,11 @@ public class FileUploadServlet extends HttpServlet {
     response.setStatus(200);
     response.setContentType("application/json");
 
+    JSONObject obj = new JSONObject();
+    obj.put("name", "jonnyboi");
+    PrintWriter out = response.getWriter();
+    out.print("slut");
+    out.flush();
 
     File file = new File(path+fileName);
     Shape[] shapesList;
