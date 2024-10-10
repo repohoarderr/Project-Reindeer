9954bdcd2c3c533d06b525c5cebd97e068b119fa
denneyl2711 <lukedenneymn@gmail.com>
2024-10-10 09:58:24 -0500

    Add several icons and buttons which aren't organized yet.

--- html/Front-End/client/Components/ManualFeatureSelection.jsx
+++ html/Front-End/client/Components/ManualFeatureSelection.jsx
@@ -2,15 +2,32 @@ import React from "react";
 
 export default function ManualFeatureSelection() {
     return (
-        <div className="manualFeatureSelection">
-            <button> a</button>
-            <button> a</button>
-            <button> a</button>
-            <button> a</button>
-            <button> a</button>
-            <button> a</button>
-            <button> a</button>
-            <button> a</button>
+        <div className="manual-feature-selection">
+            <button className="feature-button">1A</button>
+            <button className="feature-button">1B</button>
+            <button className="feature-button">1C</button>
+
+            <button className="feature-button">2A</button>
+            <button className="feature-button">3</button>
+            <button className="feature-button">4</button>
+
+            <button className="feature-button">6</button>
+            <button className="feature-button">7</button>
+            <button className="feature-button">8</button>
+
+            <button className="feature-button">9</button>
+            <button className="feature-button">11</button>
+            <button className="feature-button">12</button>
+
+            <button className="feature-button">13</button>
+            <button className="feature-button">14</button>
+            <button className="feature-button">15</button>
+
+            <button className="feature-button">17</button>
+            <button className="feature-button">S1</button>
+            <button className="feature-button">S2</button>
+
+            <button className="feature-button">16</button>
         </div>
 
     );
