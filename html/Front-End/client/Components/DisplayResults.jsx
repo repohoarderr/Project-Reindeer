07cf59386f0bcaa5be69f4ab73a588392c108235
denneyl2711 <lukedenneymn@gmail.com>
2024-10-19 14:49:20 -0500

    Display table results in front end.

--- html/Front-End/client/Components/DisplayResults.jsx
+++ html/Front-End/client/Components/DisplayResults.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-
-import {TreeTable} from 'primereact/treetable';
-import {Column} from 'primereact/column';
-
+import { TreeTable } from 'primereact/treetable';
+import { Column } from 'primereact/column';
 
 /**
  * DisplayResults component renders the results received from the backend
@@ -10,27 +8,53 @@ import {Column} from 'primereact/column';
  * @param {Object} props - The props passed to the component
  * @param {string} props.results - The results data to display
  */
-export default function DisplayResults({results}) {
+export default function DisplayResults({ results }) {
+    const round = (num) => parseFloat(num.toFixed(4));
+
+    const shapesToNodes = () => {
+        if (!results) return [];
+
+        return JSON.parse(results)
+            .map((shape, index) => {
+                if (shape.type === 'Line2D' || shape.type === 'Arc2D' || shape.type === 'circle') {
+                    return {
+                        key: index.toString(),
+                        data: {
+                            type: shape.type,
+                            centerX: shape.centerX ? round(shape.centerX) : '',
+                            centerY: shape.centerY ? round(shape.centerY) : '',
+                            area: shape.area ? round(shape.area): '',
+                            circumference: shape.circumference ? round(shape.circumference) : '',
+                            radius: shape.radius ? round(shape.radius) : ''
+                        },
+                    };
+                }
+                return null;
+            })
+            .filter(node => node !== null);// Remove null values
+
+    };
+
+    const treeTableData = shapesToNodes();
+    console.log("Transformed Data:", treeTableData);
+
     return (
         <div className="results">
-            {/* Display the results if they exist, or show a message otherwise */}
             {results ? (
                 <div>
                     <h2>File Upload Results:</h2>
-                    <pre>{results}</pre>
-                    {/* Use <pre> for formatting */}
-
+                    <TreeTable value={treeTableData} tableStyle={{ minWidth: '50rem' }}>
+                        <Column field="type" header="Type" expander></Column>
+                        <Column field="centerX" header="Center X"></Column>
+                        <Column field="centerY" header="Center Y"></Column>
+                        <Column field="area" header="Area"></Column>
+                        <Column field="circumference" header="Circumference"></Column>
+                        <Column field="radius" header="Radius"></Column>
+                    </TreeTable>
                 </div>
             ) : (
                 <div>
                     <h2>No Results Available</h2>
-
-
-                    <TreeTable tableStyle={{minWidth: '50rem'}}>
-                        <Column field="name" header="Name" expander></Column>
-                        <Column field="size" header="Size"></Column>
-                        <Column field="type" header="Type"></Column>
-                    </TreeTable>
                 </div>
             )}
         </div>
