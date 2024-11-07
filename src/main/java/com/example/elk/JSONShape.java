62ff27ee7e29316a482ecbed10883d3dbdc7ed96
denneyl2711 <lukedenneymn@gmail.com>
2024-11-07 15:26:23 -0600

    Radius notches are detected properly but the code is ugly.

--- src/main/java/com/example/elk/JSONShape.java
+++ src/main/java/com/example/elk/JSONShape.java
@@ -129,7 +129,7 @@ public class JSONShape {
             //output individual line data (drawing data)
             arr = new JSONArray();
             for (BasicLine line : lines){
-                if (line.doDraw()){
+                if (line.draw){
                     arr.add(writeDrawData(line.getSource(), id));
                 }
             }
